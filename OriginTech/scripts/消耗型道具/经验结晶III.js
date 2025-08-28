79509fcd935c61fe0c326efd31f6a357e684cb12
ty-playerocket <1953894097@qq.com>
2025-08-29 00:51:27 +0800

    Add files via upload

---
+++ OriginTech/scripts/消耗型道具/经验结晶III.js
@@ -0,0 +1,8 @@
+function onUse(event) {
+    const player = event.getPlayer();
+    const currentLevel = player.getLevel();
+    const inventory = player.getInventory();
+    const itemInMainHand = inventory.getItemInMainHand();
+    itemInMainHand.setAmount(itemInMainHand.getAmount() - 1);
+    player.setLevel(currentLevel + 20);
+}
