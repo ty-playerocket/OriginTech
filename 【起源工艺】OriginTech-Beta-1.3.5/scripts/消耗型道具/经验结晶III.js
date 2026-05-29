6e188247883513ca0da4d387a6df94967f974286
ty-playerocket <1953894097@qq.com>
2026-05-29 19:43:46 +0800

    Delete 【起源工艺】OriginTech-Beta-1.3.5 directory

--- 【起源工艺】OriginTech-Beta-1.3.5/scripts/消耗型道具/经验结晶III.js
+++
@@ -1,8 +0,0 @@
-function onUse(event) {
-    const player = event.getPlayer();
-    const currentLevel = player.getLevel();
-    const inventory = player.getInventory();
-    const itemInMainHand = inventory.getItemInMainHand();
-    itemInMainHand.setAmount(itemInMainHand.getAmount() - 1);
-    player.setLevel(currentLevel + 20);
-}
--- 【起源工艺】OriginTech-Beta-1.3.5/scripts/监听.js
+++
@@ -1,3 +0,0 @@
-function onEntityDamageByEntity(event) {
-
-}
